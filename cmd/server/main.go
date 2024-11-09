05701aa7f68b1bfbd0936ae4d99017ad10ec5af1
Fejiberglibstein <amthein321@gmail.com>
2024-11-09 10:15:11 -0500

    Start (very basic) server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,49 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+)
 
+const (
+	host = "0.0.0.0"
+	port = "24816"
+)
+
+type Server struct {
+	lobbies []Lobby  //TODO
+	clients []Client //TODO
+	ln      net.Listener
+}
+
+func main() {
+	ln, err := net.Listen("tcp", net.JoinHostPort(host, port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := Server{
+		lobbies: []invalid{}, //TODO
+		clients: []invalid{}, //TODO
+		ln:      ln,
+	}
+
+	startServer(server)
+
+}
+
+func startServer(server Server) {
+	for {
+		conn, err := server.ln.Accept()
+		if err != nil {
+			log.Print("Could not accept client connection: ", err)
+			continue
+		}
+
+		go startClient(conn)
+	}
+}
+
+func startClient(conn net.Conn) {
 
 }
