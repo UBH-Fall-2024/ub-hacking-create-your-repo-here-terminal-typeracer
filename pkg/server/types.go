ad4f4a91bef8a36c1e34e7dc07dcb90a0cc7e3a9
Fejiberglibstein <amthein321@gmail.com>
2024-11-09 22:09:17 -0500

    I did stuff

--- pkg/server/types.go
+++ pkg/server/types.go
@@ -8,6 +8,7 @@ import (
 	"encoding/gob"
 	"fmt"
 	"net"
+	"time"
 
 	"github.com/Fejiberglibstein/terminal-typeracer/pkg/network"
 	"github.com/charmbracelet/log"
@@ -30,6 +31,7 @@ type Lobby struct {
 	Id      LobbyID
 	Clients []*Client
 	State   LobbyState
+	commands chan func(network.Message)
 }
 
 // The server side representation of a Connected client
@@ -114,6 +116,10 @@ func (c *Client) handleMessage(message network.Message) {
 			}
 			c.SendMessage(&joinedMsg2)
 		}
+
+		if len(lobby.Clients) >= LOBBY_SIZE {
+			lobby.Start()
+		}
 	case network.Progress:
 		// TODO
 	default:
@@ -150,4 +156,36 @@ func (c *Client) Disconnect() {
 	for _, client := range lobby.Clients {
 		client.SendMessage(&message)
 	}
+}
+
+func (l *Lobby) SendMessage(message *network.Message) error {
+	for _, client := range l.Clients {
+		if err := client.SendMessage(message); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Lobby) Start() {
+
+	l.State = InGame
+
+	m := network.Message{
+		Header: uint8(network.GameStart),
+		Data:   "type this right now or else you will lose the entire game and i will cry and hate you forever because you suck at typing and you really need to do better and eat some chocolate chocolate chip ice cream cone tasty mm delicious",
+	}
+
+	if err := l.SendMessage(&m); err != nil {
+		log.Error("Could not start the server")
+	}
+
+	ticker := time.NewTicker(1 * time.Second)
+
+	quit := make(chan struct {})
+
+	go func() {
+
+	}
+
 }
